f1bc29696be7b6e1c4caa8df00926df4fcc0af40
Immanuel Raj <immanuelr44@gmail.com>
2021-03-04 18:36:28 +0530

    Add career fa icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
         <li><a href="#"><i class="fas fa-volume-up"></i> Announcements</a></li>
         <li><a href="#"><i class="far fa-calendar-alt"></i> Events</a></li>
         <li><a href="#"><i class="fas fa-chalkboard-teacher"></i> Faculty</a></li>
-        <li><a href="#">Career</a></li>
+        <li><a href="#"><i class="fas fa-cogs"></i> Career</a></li>
         <li><a href="#">Campus Life</a></li>
         <li><a href="#">Achievements</a></li>
         
