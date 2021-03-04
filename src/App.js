a2a3e0386db1ddd4407a6b5b9341db97b4d88774
vimalds_15 <antovimalands@gmail.com>
2021-03-04 12:15:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HomeScreen from './screens/HomeScreen'
+
+function App() {
+
+  return (
+    <div>
+      <HomeScreen />
+    <h2>Welcome</h2>
+    </div>
+  );
+}
+
+export default App;
+
