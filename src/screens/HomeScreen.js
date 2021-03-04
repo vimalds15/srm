4e1103aa746f3309604314d672d882919319c781
vimalds_15 <antovimalands@gmail.com>
2021-03-04 20:43:47 +0530

    .

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Admissions from '../components/Admissions'
 import Carousel from '../components/Carousel'
 import Header from '../components/Header'
 
@@ -6,7 +7,8 @@ const HomeScreen = () => {
     return (
     <div>
         <Header />
-        <Carousel />
+            <Carousel />
+            <Admissions />
      </div>
     )
 }
