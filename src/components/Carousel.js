f691b24151163269a21eb82bf6f96e01e064647c
vimalds_15 <antovimalands@gmail.com>
2021-03-04 16:00:29 +0530

    Add/Modify some carousel related stuffs

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,47 +1,51 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
+import styled from 'styled-components'
 import c1 from '../images/carousel/c1.jpg'
+import c2 from '../images/carousel/c2.jpg'
+import c4 from '../images/carousel/c4.jpg'
+
+
 
 const Caroussel = () => {
     return (
-        <div>
-            <Carousel>
+        <Container>
+            <Carousel  pause='false'>
                 <Carousel.Item interval={4000}>
                     <img
                     className="d-block w-100"
                     src={c1}
                     alt="First slide"
                     />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
                     <img
                     className="d-block w-100"
-                    src={c1}
+                    src={c2}
                     alt="Second slide"
                     />
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                    
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
                     <img
                     className="d-block w-100"
-                    src={c1}
+                    src={c4}
                     alt="Third slide"
                     />
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-        </div>
+        </Container>
     )
 }
 
 export default Caroussel
+
+
+const Container = styled.div`
+    margin:5%;
+    border-radius:30px;
+    overflow:hidden;
+    -webkit-box-shadow: 0px 17px 31px 9px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 17px 31px 9px rgba(0,0,0,0.75);
+box-shadow: 0px 17px 31px 9px rgba(0,0,0,0.75);
+`
