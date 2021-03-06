439342685fc9edc43bca22e266620fac4501ded7
Immanuel Raj <immanuelr44@gmail.com>
2021-03-06 16:44:17 +0530

    Add indian admission stuffs

--- src/components/Admissions.js
+++ src/components/Admissions.js
@@ -101,6 +101,20 @@ const Admissions = () => {
                     <ul>
                         For Engineering Admissions you must meet the following requirements
                         </ul>
+                        <p>
+                        You need to write an entrance exam called SRMJEEE to be eligable to move forward to the next step
+                        the exam should be undertake by students who is willing to learn <p></p>
+                        <p>Engeineering</p>
+                        <p>Medical & Health Science</p>
+                        <p>Management</p>
+                        <p>Science & Humanities</p>
+                        <p></p>
+                        Also counselling will be conducted as per marks
+                        <p></p>
+                        Click <a href="https://www.srmist.edu.in/admission-india/"><u>HERE</u></a> for more info:
+                        <p></p><p></p>
+                        <h7>*Scholrship available --Terms & Conditions apply*</h7>
+                        </p>
                     </Modal.Body>
                 </Modal>
             }
