bfc6474f654344ecee23eca58a545933150f70e9
Immanuel Raj <immanuelr44@gmail.com>
2021-03-11 11:47:15 +0530

    remove department and achivements for quic links

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,12 +13,10 @@ const Header = () => {
           <Nav.Link href="https://www.srmist.edu.in/Announcements"><i className="fas fa-volume-up"></i> Announcements</Nav.Link>
             <NavDropdown title="Quick Links" id="basic-nav-dropdown">
               <NavDropdown.Item href="https://www.srmist.edu.in/#admission"><i className="fas fa-graduation-cap"></i> Admissions</NavDropdown.Item>
-              <NavDropdown.Item href="#deptl"><i className="fas fa-building"></i> Departments</NavDropdown.Item>
             <NavDropdown.Item href="https://www.srmist.edu.in/#events"><i className="far fa-calendar-alt"></i> Events</NavDropdown.Item>
               <NavDropdown.Item href="https://www.srmist.edu.in/content/gateway-faculty"><i className="fas fa-chalkboard-teacher"></i> Faculty</NavDropdown.Item>
               <NavDropdown.Item href="http://srm.careers/"><i className="fas fa-cogs"></i> Career</NavDropdown.Item>
             <NavDropdown.Item href="https://www.srmist.edu.in/#campus"><i className="fas fa-university"></i> Campus Life</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.1"><i className="fas fa-medal"></i> Achievements</NavDropdown.Item>
               <NavDropdown.Divider />
               <div className='lgn'>
                 <NavDropdown.Item  href="#action/3.4">Login/Sign Up</NavDropdown.Item>
