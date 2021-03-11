f5652ebb189d74e5d49ce0aed6f9f4d48ef43fb6
Immanuel Raj <immanuelr44@gmail.com>
2021-03-11 17:47:05 +0530

    Add campuslife links

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
             <NavDropdown.Item href="#eventl"><i className="far fa-calendar-alt"></i> Events</NavDropdown.Item>
               <NavDropdown.Item href="https://www.srmist.edu.in/content/gateway-faculty"><i className="fas fa-chalkboard-teacher"></i> Faculty</NavDropdown.Item>
               <NavDropdown.Item href="http://srm.careers/"><i className="fas fa-cogs"></i> Career</NavDropdown.Item>
-            <NavDropdown.Item href="https://www.srmist.edu.in/#campus"><i className="fas fa-university"></i> Campus Life</NavDropdown.Item>
+            <NavDropdown.Item href="#camlife"><i className="fas fa-university"></i> Campus Life</NavDropdown.Item>
                 <NavDropdown.Item  href="https://www.srmist.edu.in/content/admission-india-portal"><i class="fas fa-user-graduate"></i> Admission enquiry</NavDropdown.Item>
             </NavDropdown>
           </Nav>
